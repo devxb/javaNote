91da237827e2ffae8f0423872294f864dfd87616
xb205 <62425964+devxb@users.noreply.github.com>
2023-04-20 21:54:05 +0900

    jacoco? (#15)
    * feat : hexagonal example
    
    * please...
    
    * jacoco?
    
    * add-jacoco-aggregation

---
+++ clean-architecture/hexagonal/user/domain/src/test/java/org/example/user/domain/entity/JacocoTest.java
@@ -0,0 +1,14 @@
+package org.example.user.domain.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class JacocoTest{
+
+    @Test
+    @DisplayName("Jacoco reports가 만들어지나?")
+    void JACOCO_REPORTS_MAKE_TEST(){
+        User user = new User(1, "Jacoco");
+    }
+
+}
