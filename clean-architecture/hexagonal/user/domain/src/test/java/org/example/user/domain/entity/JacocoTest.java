7bbdf4b058e9094b2535013b3d6f79e92e1498f3
xb205 <62425964+devxb@users.noreply.github.com>
2023-05-03 02:57:48 +0900

    Sonar 9.9.1 (#19)
    Co-authored-by: devxb <devxb@naver.com>

--- clean-architecture/hexagonal/user/domain/src/test/java/org/example/user/domain/entity/JacocoTest.java
+++ clean-architecture/hexagonal/user/domain/src/test/java/org/example/user/domain/entity/JacocoTest.java
@@ -3,13 +3,11 @@ package org.example.user.domain.entity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class JacocoTest{
+public class JacocoTest{
 
     @Test
     @DisplayName("Jacoco reports가 만들어지나?")
-    void JACOCO_REPORTS_MAKE_TEST(){
-
-
+    public void JACOCO_REPORTS_MAKE_TEST(){
 
         User user = new User(1, "Jacoco");
     }
